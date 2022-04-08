1e821e6637d414fcb6139d1167b6ce4607fbc259
ftsuda-senac <ftsuda.senac@gmail.com>
2022-04-07 22:36:57 -0300

    Ajuste no Controller i18n

--- exemplos-spring-sessao/src/main/java/br/senac/tads/dsw/exemplosspring/i18n/ComInternacionalizacaoController.java
+++ exemplos-spring-sessao/src/main/java/br/senac/tads/dsw/exemplosspring/i18n/ComInternacionalizacaoController.java
@@ -4,10 +4,15 @@
  */
 package br.senac.tads.dsw.exemplosspring.i18n;
 
+import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/com-i18n")
@@ -16,5 +21,14 @@ public class ComInternacionalizacaoController {
     @GetMapping
     public ModelAndView mostrarTela() {
         return new ModelAndView("i18n/tela-com-i18n").addObject("info", new Info());
+    }
+    
+    @PostMapping
+    public ModelAndView salvar(@ModelAttribute("info") @Valid Info info, BindingResult bindingResult,
+            RedirectAttributes redirAttr) {
+        if (bindingResult.hasErrors()) {
+            return new ModelAndView("i18n/tela-com-i18n");
+        }
+        return new ModelAndView("redirect:/com-i18n");
     }
 }

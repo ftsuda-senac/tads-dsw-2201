48e31a8fc13d42720bcad973e36be575809ac456
ftsuda-senac <ftsuda.senac@gmail.com>
2022-05-26 22:37:05 -0300

    Implementação do CRUD básico

---
+++ exemplo-rest/src/main/java/br/senac/tads/dsw/exemplorest/controller/DadosPessoaisRestController.java
@@ -0,0 +1,87 @@
+package br.senac.tads.dsw.exemplorest.controller;
+
+import br.senac.tads.dsw.exemplorest.dominio.DadosPessoais;
+import br.senac.tads.dsw.exemplorest.dominio.DadosPessoaisService;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import javax.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/rest/pessoas")
+public class DadosPessoaisRestController {
+
+    private DadosPessoaisService service;
+
+    public DadosPessoaisRestController(DadosPessoaisService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public Page<DadosPessoais> listar(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "qtde", defaultValue = "10") int qtde,
+            @RequestParam(name = "interessesIds", required = false) List<Integer> interessesIds) {
+        Page<DadosPessoais> resultadosPagina = service.findAll(page, qtde, interessesIds);
+        return resultadosPagina;
+    }
+
+    @GetMapping("/{id}")
+    public DadosPessoais formAlterar(@PathVariable("id") Integer id) {
+        Optional<DadosPessoais> optPessoa = service.findById(id);
+        if (!optPessoa.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pessoa com ID " + id + " não encontrada");
+        }
+        DadosPessoais resultado = optPessoa.get();
+        return resultado;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> salvarNovo(@RequestBody @Valid DadosPessoais dados, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Erro nos dados enviados");
+        }
+        service.save(dados);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}").buildAndExpand(dados.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity salvarExistente(@PathVariable("id") Integer id, @RequestBody @Valid DadosPessoais dados, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Erro nos dados enviados");
+        }
+        dados.setId(id);
+        service.save(dados);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity excluirExistente(@PathVariable("id") Integer id) {
+        Optional<DadosPessoais> optPessoa = service.findById(id);
+        if (!optPessoa.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pessoa com ID " + id + " não encontrada");
+        }
+        service.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
